fc1537fa95de0035c6f473656c5620aafe9e6335
Martin Beckmann <gn#36@phpbb.de>
2016-01-14 23:04:39 +0100

    First working version

--- event/listener.php
+++ event/listener.php
@@ -68,6 +68,9 @@ class listener implements EventSubscriberInterface
 	/** @var string */
 	protected $cal_event_table;
 
+	/** @var string */
+	protected $cal_participants_table;
+
 	/**
 	 * Constructor
 	 *
@@ -83,7 +86,7 @@ class listener implements EventSubscriberInterface
 	 * @param string $phpEx
 	 * @param string $hookup_path
 	 */
-	 function __construct(\gn36\hookup\functions\hookup $hookup, \phpbb\template\template $template, \phpbb\db\driver\driver_interface $db, \phpbb\user $user, \phpbb\auth\auth $auth, \phpbb\request\request_interface $request, \phpbb\event\dispatcher_interface $phpbb_dispatcher, \phpbb\notification\manager $notification_manager, $phpbb_root_path, $phpEx, $hookup_path, $cal_table, $cal_event_table)
+	 function __construct(\gn36\hookup\functions\hookup $hookup, \phpbb\template\template $template, \phpbb\db\driver\driver_interface $db, \phpbb\user $user, \phpbb\auth\auth $auth, \phpbb\request\request_interface $request, \phpbb\event\dispatcher_interface $phpbb_dispatcher, \phpbb\notification\manager $notification_manager, $phpbb_root_path, $phpEx, $hookup_path, $cal_table, $cal_event_table, $cal_participants_table)
 	 {
 		$this->hookup = $hookup;
 		$this->template = $template;
@@ -98,6 +101,7 @@ class listener implements EventSubscriberInterface
 		$this->notification_manager = $notification_manager;
 		$this->cal_table = $cal_table;
 		$this->cal_event_table = $cal_event_table;
+		$this->cal_participants_table = $cal_participants_table;
 	}
 
 	public function activedate_set($event)
@@ -105,24 +109,9 @@ class listener implements EventSubscriberInterface
 		$topic_data = $event['topic_data'];
 		$first_post = intval($topic_data['topic_first_post_id']);
 
-		if (0 == $event['active_date'])
+		if (0 == $event['set_active'])
 		{
-			// Reset
-			$sql = 'SELECT event_id FROM ' . $this->cal_table . ' WHERE post_id = ' . $first_post;
-
-			$result = $this->db->sql_query_limit($sql, 1);
-			$event_id = $this->db->sql_fetchfield('event_id');
-			$this->db->sql_freeresult($result);
-			if (!$event_id)
-			{
-				// No event entered, simply skip
-				return;
-			}
-
-			$sql = 'DELETE FROM ' . $this->cal_table . ' WHERE event_id = ' . $event_id;
-			$this->db->sql_query($sql);
-
-			$sql = 'DELETE FROM ' . $this->cal_event_table . ' WHERE id = ' . $event_id;
+			$sql = 'DELETE FROM ' . $this->cal_table . ' WHERE post_id = ' . $first_post;
 			$this->db->sql_query($sql);
 
 			$sql = 'DELETE FROM ' . $this->cal_participants_table . ' WHERE post_id = ' . $first_post;
@@ -139,7 +128,97 @@ class listener implements EventSubscriberInterface
 				}
 				$this->hookup->load_hookup($event['topic_id']);
 			}
-			//TODO Fortsetzen
+
+			$set_date = isset($this->hookup->hookup_dates[$event['set_active']]) ? (isset($this->hookup->hookup_dates[$event['set_active']]['date_time']) ? $this->hookup->hookup_dates[$event['set_active']]['date_time'] : 0) : 0;
+			if (!$set_date)
+			{
+				// We can't enter a text without date
+				return;
+			}
+
+			// Quick & dirty: The event
+			$sql = 'SELECT id FROM ' . $this->cal_event_table . ' WHERE event = \'hookup\'';
+			$result = $this->db->sql_query_limit($sql, 1);
+			$event_id = $this->db->sql_fetchfield('id');
+			$this->db->sql_freeresult($result);
+
+			if (!$event_id)
+			{
+				$sql_ary = array(
+					'event'			=> 'hookup',
+					'participants' 	=> 1,
+				);
+				$sql = 'INSERT INTO ' . $this->cal_event_table . ' ' . $this->db->sql_build_array('INSERT', $sql_ary);
+
+				$this->db->sql_query($sql);
+				$event_id = $this->db->sql_nextid();
+			}
+
+			$sql = 'SELECT count(*) as cnt FROM ' . $this->cal_table . ' WHERE post_id = ' . $topic_data['topic_first_post_id'];
+			$result = $this->db->sql_query($sql);
+			$cnt = $this->db->sql_fetchfield('cnt');
+			$this->db->sql_freeresult($result);
+
+			$sql_ary = array(
+				'post_id' 		=> $topic_data['topic_first_post_id'],
+				'event_id' 		=> $event_id,
+				'event_name' 	=> $topic_data['topic_title'],
+				'date_from' 	=> date('Y-m-d', $set_date),
+			);
+			if ($cnt)
+			{
+				$sql = 'UPDATE ' . $this->cal_table . ' SET ' . $this->db->sql_build_array('UPDATE', $sql_ary) . ' WHERE post_id = ' . $topic_data['topic_first_post_id'];
+			}
+			else
+			{
+				$sql = 'INSERT INTO ' . $this->cal_table . ' ' . $this->db->sql_build_array('INSERT', $sql_ary);
+			}
+			$this->db->sql_query($sql);
+
+			// Participants
+			$part_ary = array(hookup::HOOKUP_YES => 'yes', hookup::HOOKUP_NO => 'no', hookup::HOOKUP_MAYBE => 'mb');
+
+			// Already entered?
+			$entered_users = array();
+			if ($cnt)
+			{
+				$sql = 'SELECT user_id FROM ' . $this->cal_participants_table . ' WHERE ' . $this->db->sql_in_set('user_id', array_keys($this->hookup->hookup_users));
+				$result = $this->db->sql_query($sql);
+				$entered_users = $this->db->sql_fetchrowset($result);
+				$this->db->sql_freeresult($result);
+			}
+
+			$sql_ary = array();
+			foreach ($this->hookup->hookup_users as $user_id => $userdata)
+			{
+				// Did the user enter anything for this date?
+				if ($this->hookup->hookup_availables[$user_id][$event['set_active']] == hookup::HOOKUP_UNSET)
+				{
+					continue;
+				}
+
+				if (in_array($user_id, $entered_users))
+				{
+					// Update instead:
+					$sql = 'UPDATE ' . $this->cal_participants_table . ' SET ' . $this->db->sql_build_array('UPDATE', array(
+						'participants' 	=> $part_ary[$this->hookup->hookup_availables[$user_id][$event['set_active']]],
+						'comment'		=> $userdata['comment'],
+						'date'			=> date('Y-m-d-H-i'),
+					)) . " WHERE user_id = $user_id AND post_id = {$topic_data['topic_first_post_id']}";
+					$this->db->sql_query($sql);
+					continue;
+				}
+
+				$sql_ary[] = array(
+					'post_id' 		=> $topic_data['topic_first_post_id'],
+					'user_id' 		=> $user_id,
+					'participants' 	=> $part_ary[$this->hookup->hookup_availables[$user_id][$event['set_active']]],
+					'comments'		=> $userdata['comment'],
+					'date'			=> date('Y-m-d-H-i'),
+				);
+			}
+
+			$this->db->sql_multi_insert($this->cal_participants_table, $sql_ary);
 		}
 	}
 }
